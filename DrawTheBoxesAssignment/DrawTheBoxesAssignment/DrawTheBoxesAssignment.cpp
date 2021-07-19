a4734634cd198a65b06ea45841133d6273b26bbf
Sarah Ziraldo <sarah.ziraldo@gmail.com>
2021-07-19 12:57:25 -0400

    Initial commit
    Assignment instructions

---
+++ DrawTheBoxesAssignment/DrawTheBoxesAssignment/DrawTheBoxesAssignment.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+void gotoXY(int x, int y)
+{
+	HANDLE hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD position = { x, y };
+	SetConsoleCursorPosition(hStdout, position);
+	CONSOLE_CURSOR_INFO cursorInfo;
+	cursorInfo.bVisible = false;
+	cursorInfo.dwSize = 10;
+	SetConsoleCursorInfo(hStdout, &cursorInfo);
+}
+
+void DrawSquare(int x, int y, int width, int height, WORD colour, int ascii)
+{
+	width *= 2;
+	HANDLE ConsoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(ConsoleHandle, colour);
+
+	//Write code here
+
+}
+
+int main()
+{
+
+	DrawSquare(10, 0, 5, 5, FOREGROUND_RED, 177);
+	DrawSquare(20, 10, 5, 5, FOREGROUND_GREEN, 177);
+	DrawSquare(30, 0, 5, 5, FOREGROUND_BLUE, 177);
+
+	gotoXY(0, 29);
+	system("pause");
+	return 0;
+}
